7b9a9d3b364d0276b0e74b91804e8e57ff7318cf
Markus Puffer <m.puffer@pm-webdesign.eu>
2020-10-05 13:41:52 +0200

    [DOCS] Change licence header.

--- Configuration/TCA/Overrides/fe_users.php
+++ Configuration/TCA/Overrides/fe_users.php
@@ -1,15 +1,28 @@
 <?php
 
-/* 
- * Copyright (c) 2018 Markus Puffer <m.puffer@pm-webdesign.eu>.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+/*
+ * Copyright (C) 2018 pm-webdesign.eu 
+ * Markus Puffer <m.puffer@pm-webdesign.eu>
  *
- * Contributors:
- *    Markus Puffer <m.puffer@pm-webdesign.eu> - initial API and implementation and/or initial documentation
+ * All rights reserved
+ *
+ * This script is part of the TYPO3 project. The TYPO3 project is
+ * free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The GNU General Public License can be found at
+ * http://www.gnu.org/copyleft/gpl.html.
+ *
+ * This script is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * This copyright notice MUST APPEAR in all copies of the script!
  */
+
 if (!defined('TYPO3_MODE')) {
     die ('Access denied.');
 }
@@ -198,7 +211,7 @@ $fields = [
                 'maxitems' => 100,   
                 
         ],
-    ],
+    ],    
 ];
 
 // Add new fields to fe_users
